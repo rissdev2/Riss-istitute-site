fd7064ac3f08be36ff8c50c8396f3d6ce9029822
rissdev2 <rissgroupdev2@gmail.com>
2022-10-05 12:41:34 +0500

    ho jaya ga

--- next.config.js
+++ next.config.js
@@ -5,15 +5,6 @@ const nextConfig = {
   compress: false,
 }
 
-module.exports = {
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    // Important: return the modified config
-    return config
-  },
-}
 
 
 module.exports = nextConfig
