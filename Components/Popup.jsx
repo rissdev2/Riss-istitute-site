7876dcaea39110d948130679b7993a1346216ef5
rissdev2 <rissgroupdev2@gmail.com>
2022-10-12 16:04:32 +0500

    replc pop img

--- Components/Popup.jsx
+++ Components/Popup.jsx
@@ -8,7 +8,7 @@ import Swal from 'sweetalert2/dist/sweetalert2.js'
 const Popup = () => {
     const Swal = require('sweetalert2')
     Swal.fire({
-        imageUrl:'https://previews.dropbox.com/p/thumb/ABoTJgxEuK3V3tO8l4YgWipJ83pI8t4Z2H7imIvdDG0TsViogZjPgQ4pAO4dxZ366ZHBViu_8Boadvc4wdBOEnaTg0fiuOg_m4vmjdJMuN7tdwH21KYh_rjkcObHNL4rxxWy8L5IoljamgDtOJaL5sUBP9j3swO6yFvFmXoZ4-pLVhEWdukG1mU_6PRpBKLdMRqHoJNbwsBwRNeA6Yu0UwVkIIiR-TiQrVSJmjjRSFJ1Jg0wEghBftMD-ixg_7bqQaD9L1f70oOH4lSGhclLg4aVDiDiHDfZqyWeAYl2gJBvs4GBHBOoVpslC3Kbdz23gKG6rYYrdvzeGFZmCr2kKVl8P638OcVGHSO2Zd3QkryipDtJJqhT4pEJWE6jR7HPKZc/p.png',
+        imageUrl:'https://previews.dropbox.com/p/thumb/ABr0m6eE5PP5cqzAgdS9I-4wbNwEIC6O_lMIWZORXounrZ8zKLUCV7rrbCCBByKY3dEQLFgZBJ0HniB2IqFYya124sJ9ECRlG8w75qJq2cBJHqGWgLvHcdzbqH45DmpJP4TqwoycWhSFfBdi2sCmS9f675cmgfgdFK8QTd2mUAwlHsU9r9TBj8x60mpLfsfld4VF3NGu8tU0bZbgSK5JHzBf5O8U_Hi6rn5-mEHbTCeDjFXZWbyQ1QuEM33hS0dRixrv7lIhycRWOc-4d4ul6fm3MhW8SBx9hgPUP4NmxPdj6yqLmYJAgqo8UTlJGaLTYLgEZnvXoRSHiXz0huMq2p9B4IP8wkJGUk6JC6aZp5jrRTUIYzQW3CG9BUF3-IL8E78/p.png',
         buttons: ' false',
         showCloseButton: 'true',
         showConfirmButton: false,
