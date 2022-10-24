cc700938116e78ad50119b17dffec7e412debdf5
rissdev2 <104282927+rissdev2@users.noreply.github.com>
2022-10-24 15:41:48 +0500

    new annoucement

--- Components/Popup.jsx
+++ Components/Popup.jsx
@@ -8,7 +8,7 @@ import Swal from 'sweetalert2/dist/sweetalert2.js'
 const Popup = () => {
     const Swal = require('sweetalert2')
     Swal.fire({
-        imageUrl:'https://theriss.net/institute_website_announcement/ITInstitute.jpg',
+        imageUrl:'https://theriss.net/institute_website_announcement/ITCourses.png',
         buttons: ' false',
         showCloseButton: 'true',
         showConfirmButton: false,
